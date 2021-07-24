6ce69635b5e9ffe45f91b46ea78f8f1385026068
seee-shun <seino.shungo.17@shizuoka.ac.jp>
2021-07-24 17:19:29 +0900

    heroku linebot連携

--- server.js
+++ server.js
@@ -2,14 +2,14 @@
 
 const express = require("express");
 // const mysql = require("mysql");
-// const line = require("@line/bot-sdk");
+const line = require("@line/bot-sdk");
 const PORT = process.env.PORT || 5000;
 
-// const config = {
-//   channelSecret: "356084186cc5e93d0d8f4b1c896b384a",
-//   channelAccessToken:
-//     "LBuTK2807CaJXrJ4Bkl6YnJ9/uOn/EPseaViFzNjt1ap5CgGy6pJSMwji9aUqQ6a/uDjrk372f8s8DjuvBHg2qAxVHu3vuX54x3oj8ZJEuPEUQVEpqkTmhW3y+G++z5uvymYMRt4tsK+eBKWVq0I4AdB04t89/1O/w1cDnyilFU=",
-// };
+const config = {
+  channelSecret: "356084186cc5e93d0d8f4b1c896b384a",
+  channelAccessToken:
+    "LBuTK2807CaJXrJ4Bkl6YnJ9/uOn/EPseaViFzNjt1ap5CgGy6pJSMwji9aUqQ6a/uDjrk372f8s8DjuvBHg2qAxVHu3vuX54x3oj8ZJEuPEUQVEpqkTmhW3y+G++z5uvymYMRt4tsK+eBKWVq0I4AdB04t89/1O/w1cDnyilFU=",
+};
 
 const app = express();
 
@@ -37,33 +37,33 @@ const app = express();
 // });
 
 app.get("/", (req, res) => res.send("Hello LINE BOT!(GET)")); //ブラウザ確認用(無くても問題ない)
-// app.post("/webhook", line.middleware(config), (req, res) => {
-//   console.log(req.body.events);
-//   Promise.all(req.body.events.map(handleEvent)).then((result) =>
-//     res.json(result)
-//   );
-// });
+app.post("/webhook", line.middleware(config), (req, res) => {
+  console.log(req.body.events);
+  Promise.all(req.body.events.map(handleEvent)).then((result) =>
+    res.json(result)
+  );
+});
 
-// const client = new line.Client(config);
+const client = new line.Client(config);
 
-// const handleEvent = async (e) => {
-//   if (e.type !== "message" || e.message.type !== "text") {
-//     return Promise.resolve(null);
-//   }
+const handleEvent = async (e) => {
+  if (e.type !== "message" || e.message.type !== "text") {
+    return Promise.resolve(null);
+  }
 
-//   if (e.message.text === "明日のごみは？") {
-//     // replyText = "明日はかん、ペットボトルがゴミ出しの日です";
-//   } else if (e.message.text === "地域を変更") {
-//     // replyText = "特になし";
-//   } else if (e.message.text === "通知時間を変更") {
-//     // replyText = "通知時間を変更してください";
-//   }
+  if (e.message.text === "明日のごみは？") {
+    replyText = "明日はかん、ペットボトルがゴミ出しの日です";
+  } else if (e.message.text === "地域を変更") {
+    replyText = "特になし";
+  } else if (e.message.text === "通知時間を変更") {
+    replyText = "通知時間を変更してください";
+  }
 
-//   return client.replyMessage(e.replyToken, {
-//     type: "text",
-//     text: replyText,
-//   });
-// };
+  return client.replyMessage(e.replyToken, {
+    type: "text",
+    text: replyText,
+  });
+};
 
 // const toMessage = () => {
 //   client.pushMessage("U1221c5a7f6d56970a7dce56d99a5a9ae", {
