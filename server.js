d3d35d57d06d2380317af1ce396ee2efb2e9b994
seee-shun <seino.shungo.17@shizuoka.ac.jp>
2021-07-31 05:45:47 +0900

    log確認

--- server.js
+++ server.js
@@ -118,6 +118,7 @@ const handleEvent = async (e) => {
     const month_zero = ("00" + month).slice(-2);
     const day_zero = ("00" + day).slice(-2);
     const tomorrow = "2021-" + month_zero + "-" + day_zero;
+    console.log(e.source.userId);
 
     connection.query(
       `SELECT livedArea from users WHERE userId = '${e.source.userId}'`,
