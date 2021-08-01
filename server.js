a084103646d24fefc9af556385aa86a52cd5a316
seee-shun <seino.shungo.17@shizuoka.ac.jp>
2021-08-01 09:43:45 +0900

    webhookのエンドポイントテスト

--- server.js
+++ server.js
@@ -43,6 +43,7 @@ const handleDisconnect = () => {
 
 handleDisconnect();
 
+app.get("/", (req, res) => res.send("Hello LINE BOT!(GET)"));
 app.post("/webhook", line.middleware(config), (req, res) => {
   console.log(req.body.events);
   Promise.all(req.body.events.map(handleEvent)).then((result) => {
